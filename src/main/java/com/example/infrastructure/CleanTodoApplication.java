24b07b0d6b296c198ed10bf1ca6278384f7c5fe0
LEEJ0NGWAN <lastffx@gmail.com>
2022-10-16 20:43:57 +0900

    feat: spring boot application(#4)

---
+++ src/main/java/com/example/infrastructure/CleanTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanTodoApplication.class, args);
+	}
+
+}
