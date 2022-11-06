2d4bc33e563ce0bc3ce9f399434030cd521596e9
LEEJ0NGWAN <lastffx@gmail.com>
2022-11-06 22:28:03 +0900

    feat: login success handler (#4)

---
+++ src/main/java/com/example/infrastructure/security/LoginSuccessHandler.java
@@ -0,0 +1,26 @@
+package com.example.infrastructure.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+public class LoginSuccessHandler
+extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Authentication authentication) throws ServletException, IOException {
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        response.sendRedirect("/main");
+    }
+}
