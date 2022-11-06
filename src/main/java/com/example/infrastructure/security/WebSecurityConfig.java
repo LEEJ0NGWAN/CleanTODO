0d9f3e6d8b0e0eb15a53bce27ea29ffac5f649f3
LEEJ0NGWAN <lastffx@gmail.com>
2022-11-06 22:28:32 +0900

    feat: web security config (#4)

---
+++ src/main/java/com/example/infrastructure/security/WebSecurityConfig.java
@@ -0,0 +1,72 @@
+package com.example.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(WebSecurity web) {
+
+        web
+        .ignoring()
+        .antMatchers("/css/**", "/h2-console/**");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http
+        .csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/login", "/signup").permitAll()
+        .antMatchers("/**").authenticated()
+        .and()
+        .formLogin().loginPage("/login")
+        .successHandler(loginSuccessHandler())
+        .failureForwardUrl("/fail")
+        .and()
+        .logout()
+        .logoutUrl("/logout");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public PasswordAuthenticationProvider
+    passwordAuthenticationProvider() {
+
+        return new PasswordAuthenticationProvider(
+            userDetailsService, bCryptPasswordEncoder());
+    }
+
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler() {
+
+        return new LoginSuccessHandler();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder builder) {
+
+        builder.authenticationProvider(passwordAuthenticationProvider());
+    }
+}
