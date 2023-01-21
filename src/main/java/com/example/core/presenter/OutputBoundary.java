1862d3a236c29547ca6a4ccc4da97d1f213047dc
LEEJ0NGWAN <lastffx@gmail.com>
2023-01-21 12:02:57 +0900

    refactor: rename a parameter for output boundary present method (#3)

--- src/main/java/com/example/core/presenter/OutputBoundary.java
+++ src/main/java/com/example/core/presenter/OutputBoundary.java
@@ -4,5 +4,5 @@ import com.example.core.usecase.InputBoundary;
 
 public interface OutputBoundary<R extends InputBoundary.ResponseModel> {
 
-    public void present(R responseModel);
+    public void present(R model);
 }
